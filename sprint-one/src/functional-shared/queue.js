aba8b0586247d3923cbf023269d7d2096ea5dde4
Hack Reactor Students <accounts@hackreactor.com>
2015-08-19 21:38:15 -0700

    implemented queue all tests passed

--- sprint-one/src/functional-shared/queue.js
+++ sprint-one/src/functional-shared/queue.js
@@ -1,8 +1,32 @@
 var Queue = function(){
   // Hey! Rewrite in the new style. Your code will wind up looking very similar,
   // but try not not reference your old code in writing the new style.
+  var obj = {};
+  obj.storage = {};
+  obj.length = 0;
+  //to point at most bottom value:
+  obj.head = 0;
+  _.extend(obj, queueMethods);
+
+  return obj;
 };
 
-var queueMethods = {};
+var queueMethods = {
+  enqueue : function(val){
+    this.storage[this.length + this.head] = val;
+    this.length++;
+  },
+
+  dequeue : function(){
+    if (this.length) {
+      this.length--;
+      return this.storage[this.head++];
+    };
+  },
+
+  size : function(){
+    return this.length;
+  }
+};
 
 
