ac781adf5aa7861bb77502495259346aad4aa604
youngfreezy <Fareez.Ahmed@gmail.com>
2015-08-19 21:38:15 -0700

    implemented queue all tests passed

--- sprint-one/src/prototypal/queue.js
+++ sprint-one/src/prototypal/queue.js
@@ -1,8 +1,30 @@
 var Queue = function() {
   // Hey! Rewrite in the new style. Your code will wind up looking very similar,
   // but try not not reference your old code in writing the new style.
+  var obj = Object.create(queueMethods);
+
+  obj.storage = {};
+  obj.length = 0;
+  obj.head = 0;
+
+  return obj;
 };
 
-var queueMethods = {};
+var queueMethods = {
+  //FIFO
+  enqueue: function(val){
+    this.storage[this.length + this.head] = val;
+    this.length++;
+  }, 
+  dequeue: function(){
+    if (this.length) {
+      this.length--;
+      return this.storage[this.head++];
+    };
+  },
+  size: function(){
+    return this.length;
+  }
+};
 
 
--- sprint-one/src/pseudoclassical/queue.js
+++ sprint-one/src/pseudoclassical/queue.js
@@ -1,6 +1,25 @@
 var Queue = function() {
+  this.storage = {};
+  this.length = 0;
+  this.head = 0;
   // Hey! Rewrite in the new style. Your code will wind up looking very similar,
   // but try not not reference your old code in writing the new style.
 };
 
+//enqueue, dequeue, size
+Queue.prototype.enqueue = function(val){
+  this.storage[this.length + this.head] = val;
+  this.length++;
+}
 
+Queue.prototype.dequeue = function(){
+  if (this.length) {
+    this.length--;
+    return this.storage[this.head++];
+  };
+}
+
+Queue.prototype.size = function(){
+  return this.length;
+}
+//make sure to call with new. that's what makes the this references work!
