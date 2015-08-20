4c195471e7a79d893ef484f62f8951912a95e5e9
youngfreezy <Fareez.Ahmed@gmail.com>
2015-08-19 20:59:42 -0700

    Implemented prototypal and pseudo stack data structs

--- sprint-one/src/prototypal/stack.js
+++ sprint-one/src/prototypal/stack.js
@@ -1,8 +1,33 @@
 var Stack = function() {
   // Hey! Rewrite in the new style. Your code will wind up looking very similar,
   // but try not not reference your old code in writing the new style.
+  var obj = Object.create(stackMethods);
+  obj.length = 0;
+  obj.storage = {};
+  
+  return obj;
 };
 
-var stackMethods = {};
+var stackMethods = {
+
+  pop : function(){
+    if (this.length > 0) {
+      this.length--;
+      return this.storage[this.length];
+    }
+  },
+
+  push : function(val){
+    this.storage[this.length] = val;
+    this.length++;
+  },
+
+  size : function(){
+    return this.length;
+  }
+}
+
+
+
 
 
--- sprint-one/src/pseudoclassical/stack.js
+++ sprint-one/src/pseudoclassical/stack.js
@@ -1,6 +1,31 @@
 var Stack = function() {
   // Hey! Rewrite in the new style. Your code will wind up looking very similar,
   // but try not not reference your old code in writing the new style.
+
+  //create a storage and length;
+  this.storage = {};
+  this.length = 0;
+
+ 
+}
+
+ //LIFO: pushing on the top
+Stack.prototype.push = function(val){
+
+  this.storage[this.length] = val;
+    this.length++;
+};
+  //popping of the top.
+Stack.prototype.pop = function(){
+  if (this.length) {
+    this.length--;
+    return this.storage[this.length];
+  };
 };
 
+Stack.prototype.size = function(){
+  return this.length;
+}
 
+
+// call like this: var stack = new Stack():
